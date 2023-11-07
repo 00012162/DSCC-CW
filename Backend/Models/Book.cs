5a1b1a962cf7d4bbb8903e7b887208beb6fe6771
Sarvarbek Abdullaev <00012162@wiut.uz>
2023-11-07 13:00:57 +0500

    feat: migrations & dbContext

---
+++ Backend/Models/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace Backend.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int? Pages { get; set; }
+        public Writer? Writer { get; set; }
+    }
+}
